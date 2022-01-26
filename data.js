a9662a3d96e90377eacc4327b41486cf0bcbfbe1
Fat Han Nuraddin <fat.han.nuraddin@gmail.com>
2022-01-26 15:54:24 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,46 @@
+var slide = [
+	{
+		// telegram rest title
+		'p':"p.2.1.html"
+	},
+	{
+		// telegram rest
+		'p':"p.2.2.html"
+	},
+	{
+		// 502
+		'p':"p.2.3.html"
+	},
+	{
+		// tdlight title
+		'p':"p.first.html"
+	},
+
+	{
+		// tdlight
+		'p':"p.1.1.html"
+	},
+	{
+		// user mode
+		'p':"p.1.2.html"
+	},
+
+	{
+		// user vs bot
+		'p':"p.1.3.html"
+	},
+	{
+		// user vs bot
+		'p':"p.1.4.html"
+	},
+	{
+		// demo
+		'p':"p.demo.html"
+	},
+
+	{
+		// thank you
+		'p':"p.last.html"
+	}
+];
+
